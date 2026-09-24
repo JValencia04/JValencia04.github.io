2917983db4313c332ab7f00e2b10dbe6d2898105
JValencia04 <ja.valenciaa@duocuc.cl>
2026-09-24 14:46:09 -0300

    Implementacion de Footer para el npm run dev

---
+++ src/core/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    const currentYear = new Date().getFullYear();
+    return(
+        <footer>
+            <p>{currentYear} Portafolio 2026</p>
+        </footer>
+    )
+}
+export default Footer;
